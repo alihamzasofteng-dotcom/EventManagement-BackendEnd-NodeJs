6cf252d0570040dd43fa27bc4b33928e0cd0cad6
Hamza Ali <Muhammadhamzatariq786@gmail.com>
2024-10-14 22:26:07 +0500

    first commit

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ require('./startup/config')();
 require('./startup/validation')();
 require('./startup/prod')(app);
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
 const port = process.env.PORT || 3000;
 const server =app.listen(port, () => winston.info(`Listening on port ${port}...`));
 // integration testing ky lye server ko export kr rhy han test file main import ky liye
